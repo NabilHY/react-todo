5989a0437d2d29d0260236a324a2295634d18669
nabsternab <hayounn.etd@gmail.com>
2022-06-01 10:37:34 +0100

    React Routing

--- src/classBased/components/InputTodo.js
+++ src/classBased/components/InputTodo.js
@@ -1,42 +1,42 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
-    state = {
-        title: ""
-    };
+  state = {
+    title: '',
+  };
 
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
-    };
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        if (this.state.title.trim()) {
-            this.props.addTodoProps(this.state.title)
-            this.setState({
-              title: "",
-            })
-          } else {
-            alert("Please write item")
-          }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
     }
+  };
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    className="input-text"
-                    placeholder="Add Todo..."
-                    onChange={this.onChange}
-                    name="title"
-                    value={this.state.title}
-                />
-                <button>Submit</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          onChange={this.onChange}
+          name="title"
+          value={this.state.title}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
-export default InputTodo
+export default InputTodo;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TodoContainer from "./functionBased/components/TodoContainer"
-import "./functionBased/App.css";
+/* eslint-disable */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './functionBased/App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <TodoContainer />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
